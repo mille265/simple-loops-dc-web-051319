3250d0e7cb3bf72e807a0aa8fd42753590f16b31
meghan miller <mille265@gmail.com>
2019-05-07 17:40:19 +0000

    Done.

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -1,37 +1,62 @@
-require_relative './spec_helper'
+# REMEMBER: print your output to the terminal using 'puts'
 
-describe 'Loops' do
-  before(:each) do
-    @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
+def loop_iterator(number_of_times)
+  # Code your solution here using the "loop" keyword to puts out the below phrase
+  # This and all the other methods take an argument of an integer
+  # The integer is the number of times the loops should puts out the phrase
+  # How can we make sure the loop breaks once it has puts out the phrase the
+  #   correct number of times?
+  # Maybe we should keep count of the number of times we've puts out the
+  #  phrase and break when the counter hits the appropriate number...
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  counter = 0
+  loop do
+    puts phrase
+    counter += 1
+    break if counter == number_of_times
   end
+end
 
-  describe 'loop' do
-    it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
-      expect{ loop_iterator(7) }.to output(@looping_string).to_stdout
-    end
+def times_iterator(number_of_times)
+  # code your solution here using the "times" keyword
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do
+    puts phrase
   end
+end
 
-  describe 'times' do
-    it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
-      expect{ times_iterator(7) }.to output(@looping_string).to_stdout
-    end
+def while_iterator(number_of_times)
+  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  count = 1
+  while count <= number_of_times
+    puts phrase
+    count += 1
   end
+end
 
-  describe 'while' do
-    it 'prints out Welcome to Flatiron School\'s Web Development Course! 7 times' do
-      expect{ while_iterator(7) }.to output(@looping_string).to_stdout
-    end
+def until_iterator(number_of_times)
+  # code your solution here using the "until" keyword
+  # hint: use a counter to tell the until loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  count = 1
+  until count > number_of_times
+    puts phrase
+    count += 1
   end
+end
 
-  describe 'until' do
-    it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
-      expect{ until_iterator(7) }.to output(@looping_string).to_stdout
-    end
-  end
+def for_iterator(number_of_times)
+  # code your solution here using the "for" keyword
+  # remember that `for` requires a range of numbers. How can we turn our number into a range?
 
-  describe 'for' do
-    it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
-      expect{ for_iterator(7) }.to output(@looping_string).to_stdout
-    end
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  for count in 1..number_of_times do
+    puts phrase
   end
 end
